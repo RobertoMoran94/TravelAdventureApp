da2fc7f4f1213736ffad0befb37c161e76819499
RobertoMoran94 <robertomoran94@hotmail.com>
2024-03-08 23:17:50 -0500

    Adding MoviesNetworkLayer

--- TravelAdventureApp/Models/Movie.swift
+++ TravelAdventureApp/Models/Movie.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct MoviesResponse: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Decodable {
+    let adult: Bool?
+    let backdrop_path: String?
+    let id: Int?
+    let title: String?
+    let original_language: String?
+    let original_title: String?
+    let overview: String?
+    let poster_path: String?
+    let media_type: String?
+    let genre_ids: [Int]?
+    let popularity: Double?
+    let release_date: String?
+    let video: Bool?
+    let vote_average: Double?
+    let vote_count: Int?
+}
